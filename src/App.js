b108c1b385eb8840d0992f00ac014d7f85caf083
Isioye Mohammed <mohammed.isioye@gmail.com>
2019-03-04 15:10:28 +0100

    Generate Phone Numbers
    - add error component
    - update input component
    - update app stylesheet
    - add generate phone number function to App.js
    - update phone number component

--- src/App.js
+++ src/App.js
@@ -5,21 +5,74 @@ import SubHeader from './components/Header/SubHeader'
 import Input from './components/Input/Input';
 import PhoneNumbers from './components/PhoneNumbers/PhoneNumber';
 import SortBy from './components/Sort/SortBy';
+import Error from './components/Error/Error';
 
 
 class App extends Component {
+  state = {
+    error: false,
+    message: "",
+    bound: 1,
+    phoneNumbers: []
+  };
+
+  handleNumberGeneration = event => {
+    event.preventDefault();
+    const { bound } = this.state;
+    if (bound > 10000) return this.setState({
+      error: true,
+      message: "Please enter a number less than 10000"
+    });
+    let phoneNumbers = [];
+    let phoneNumber = 0;
+    while (phoneNumber < bound) {
+      phoneNumbers.push('0' + Math.floor(Math.random() * 900000000 + 100000000));
+      phoneNumber++;
+    }
+    return this.setState({
+      phoneNumbers
+    });
+  };
+
+  handleInput = async event => {
+    event.preventDefault();
+    const bound = event.target.value;
+    try {
+      if (Math.floor(Number(bound))) {
+        this.setState({
+          bound
+        })
+      }
+    } catch (e) {
+      this.setState({
+        error: true,
+        message: "An error occurred during this operation"
+      })
+    }
+  };
+
   render() {
+    const { error, message, phoneNumbers } = this.state;
     return (
       <Fragment>
         <Header />
         <div className="wrapper">
           <SubHeader />
           <div className="App-body">
+            <Error
+              error={error}
+              message={message}
+            />
             <div className="input-wrapper">
-              <Input />
+              <Input
+                onClick={this.handleNumberGeneration}
+                onChange={this.handleInput}
+              />
               <SortBy />
             </div>
-            <PhoneNumbers />
+            <PhoneNumbers
+              phoneNumbers={phoneNumbers}
+            />
           </div>
         </div>
       </Fragment>
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-
-const Input = () => (
+const Input = ({ onClick, onChange }) => (
   <div>
     <input
       type="number"
       placeholder="Enter a number"
+      onChange={onChange}
     />
-    <button>Generate</button>
+    <button onClick={onClick}>Generate Phone Number(s)</button>
   </div>
 );
+
+Input.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
 
 export default Input;
