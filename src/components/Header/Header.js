b9a287903a4c1654e341d22dc8b1cd6b6a6c2955
Isioye Mohammed <mohammed.isioye@gmail.com>
2019-03-04 18:01:11 +0100

    Display max and min Phone Numbers
    - add min-max function to app.js
    - update style sheet
    - update the header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
-const Header = () => (
+const Header = ({ min, max, total, phoneNumbers }) => (
   <header className="App-header">
     <h1>Phone Number Generator</h1>
-    <h3>Details</h3>
-    <div className="sort-details">
-      <p><strong>Min Phone number:</strong> 0898776765</p>
-      <p><strong>Max Phone number:</strong> 0898776765</p>
-      <p><strong>Total number of Phone numbers:</strong> 5</p>
-    </div>
+      {
+        phoneNumbers.length > 0 &&
+          <Fragment>
+            <h3>Details</h3>
+            <div className="sort-details">
+              <p><strong>Min Phone number:</strong> {min}</p>
+              <p><strong>Max Phone number:</strong> {max}</p>
+              <p><strong>Total number of Phone numbers:</strong> {total}</p>
+            </div>
+          </Fragment>
+      }
   </header>
 );
+
+Header.propTypes = {
+  min: PropTypes.number,
+  max: PropTypes.number,
+  total: PropTypes.number,
+  phoneNumbers: PropTypes.array.isRequired,
+};
 
 export default Header;
